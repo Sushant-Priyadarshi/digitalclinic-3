00d021f1a669890478771df64402dece7275dc6f
Sushant Priyadarshi <sp9608165373@gmail.com>
2026-03-12 00:05:14 +0530

    Create doctors route page

---
+++ app/doctors/page.js
@@ -0,0 +1,8 @@
+export default function Doctors() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Find Doctors</h1>
+      <p>Doctor discovery page coming soon.</p>
+    </div>
+  );
+}
